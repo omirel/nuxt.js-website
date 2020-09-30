5db9f024f5313451707616c213f281efbf351866
omirel <olaf@mirel.de>
2020-09-30 16:30:15 +0200

    changed Keys

--- config/production.js
+++ config/production.js
@@ -1,7 +1,7 @@
 module.exports = {
   cosmic: {
     bucket: {
-      slug: process.env.COSMIC_BUCKET || 'nuxt-website',
+      slug: process.env.COSMIC_BUCKET_SLUG || 'nuxt-website',
       read_key: process.env.COSMIC_READ_KEY,
       write_key: process.env.COSMIC_WRITE_KEY
     }
