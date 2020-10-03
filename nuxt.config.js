3b878dc317fd50c7ae75b468e3c2841bc4de2f2f
Olaf Mirel <olaf@mirel.de>
2020-10-03 21:13:06 +0200

    reduced css

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,24 +2,24 @@ export default {
 	head: {
 		title: "My Nuxt.js Application!",
 		script: [
-			// { src: '/vendor/modernizr/modernizr.min.js', body: false, defer: true },
+			{ src: '/vendor/modernizr/modernizr.min.js', body: false, defer: true },
 			
 			// <!-- Vendor -->
-			// { src: '/vendor/jquery/jquery.min.js', body: true, defer: true },
-			// { src: '/vendor/jquery.appear/jquery.appear.min.js', body: true, defer: true },
-			// { src: '/vendor/jquery.easing/jquery.easing.min.js', body: true, defer: true },
-			// { src: '/vendor/jquery.cookie/jquery.cookie.min.js', body: true, defer: true },
-			// { src: '/vendor/popper/umd/popper.min.js', body: true, defer: true },
-			// { src: '/vendor/bootstrap/js/bootstrap.min.js', body: true, defer: true },
-			// { src: '/vendor/common/common.min.js', body: true, defer: true },
-			// { src: '/vendor/jquery.validation/jquery.validate.min.js', body: true, defer: true },
-			// { src: '/vendor/jquery.easy-pie-chart/jquery.easypiechart.min.js', body: true, defer: true },
-			// { src: '/vendor/jquery.gmap/jquery.gmap.min.js', body: true, defer: true },
-			// { src: '/vendor/jquery.lazyload/jquery.lazyload.min.js', body: true, defer: true },
-			// { src: '/vendor/isotope/jquery.isotope.min.js', body: true, defer: true },
-			// { src: '/vendor/magnific-popup/jquery.magnific-popup.min.js', body: true, defer: true },
-			// { src: '/vendor/vide/jquery.vide.min.js', body: true, defer: true },
-			// { src: '/vendor/vivus/vivus.min.js', body: true, defer: true },
+			{ src: '/vendor/jquery/jquery.min.js', body: true, defer: true },
+			{ src: '/vendor/jquery.appear/jquery.appear.min.js', body: true, defer: true },
+			{ src: '/vendor/jquery.easing/jquery.easing.min.js', body: true, defer: true },
+			{ src: '/vendor/jquery.cookie/jquery.cookie.min.js', body: true, defer: true },
+			{ src: '/vendor/popper/umd/popper.min.js', body: true, defer: true },
+			{ src: '/vendor/bootstrap/js/bootstrap.min.js', body: true, defer: true },
+			{ src: '/vendor/common/common.min.js', body: true, defer: true },
+			{ src: '/vendor/jquery.validation/jquery.validate.min.js', body: true, defer: true },
+			{ src: '/vendor/jquery.easy-pie-chart/jquery.easypiechart.min.js', body: true, defer: true },
+			{ src: '/vendor/jquery.gmap/jquery.gmap.min.js', body: true, defer: true },
+			{ src: '/vendor/jquery.lazyload/jquery.lazyload.min.js', body: true, defer: true },
+			{ src: '/vendor/isotope/jquery.isotope.min.js', body: true, defer: true },
+			{ src: '/vendor/magnific-popup/jquery.magnific-popup.min.js', body: true, defer: true },
+			{ src: '/vendor/vide/jquery.vide.min.js', body: true, defer: true },
+			{ src: '/vendor/vivus/vivus.min.js', body: true, defer: true },
 			
 		
 		// <!-- Theme Base, Components and Settings -->
@@ -56,16 +56,16 @@ export default {
 		
 		// <!-- Vendor CSS -->
 		'~/assets/vendor/bootstrap/css/bootstrap.min.css',
-		'~/assets/vendor/fontawesome-free/css/all.min.css',
+		// '~/assets/vendor/fontawesome-free/css/all.min.css',
 		'~/assets/vendor/animate/animate.compat.css',
-		'~/assets/vendor/simple-line-icons/css/simple-line-icons.min.css',
+		// '~/assets/vendor/simple-line-icons/css/simple-line-icons.min.css',
 		'~/assets/vendor/magnific-popup/magnific-popup.min.css',
 		
 		// !-- Theme CSS -->
 		'~/assets/css/theme.css',
 		'~/assets/css/theme-elements.css',
-		'~/assets/css/theme-blog.css',
-		'~/assets/css/theme-shop.css',
+		// '~/assets/css/theme-blog.css',
+		// '~/assets/css/theme-shop.css',
 		
 		// <!-- Current Page CSS -->
 		'~/assets/vendor/rs-plugin/css/settings.css',
