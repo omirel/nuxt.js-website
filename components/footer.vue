51bd96b077a0e9e9d0e552a4037f723d8dc0c3bc
Olaf Mirel <olaf@mirel.de>
2020-10-06 12:13:54 +0200

    best poractices

--- components/footer.vue
+++ components/footer.vue
@@ -65,9 +65,9 @@
               <div class="col-auto">
                 <div class="appear-animation" data-appear-animation="fadeIn" data-appear-animation-delay="100">
                   <ul class="header-social-icons social-icons social-icons-clean social-icons-icon-light social-icons-big d-none d-lg-block m-0 p-relative bottom-10">
-                    <li class="social-icons-instagram"><a href="http://www.instagram.com/" target="_blank" class="text-4" title="Instagram"><i class="fab fa-instagram"></i></a></li>
-                    <li class="social-icons-twitter"><a href="http://www.twitter.com/" target="_blank" class="text-4" title="Twitter"><i class="fab fa-twitter"></i></a></li>
-                    <li class="social-icons-facebook"><a href="http://www.facebook.com/" target="_blank" class="text-4" title="Facebook"><i class="fab fa-facebook-f"></i></a></li>
+                    <li class="social-icons-instagram"><a rel="noreferrer" href="http://www.instagram.com/" target="_blank" class="text-4" title="Instagram"><i class="fab fa-instagram"></i></a></li>
+                    <li class="social-icons-twitter"><a rel="noreferrer" href="http://www.twitter.com/" target="_blank" class="text-4" title="Twitter"><i class="fab fa-twitter"></i></a></li>
+                    <li class="social-icons-facebook"><a rel="noreferrer" href="http://www.facebook.com/" target="_blank" class="text-4" title="Facebook"><i class="fab fa-facebook-f"></i></a></li>
                   </ul>
                 </div>
               </div>
--- components/nav.vue
+++ components/nav.vue
@@ -116,9 +116,9 @@
           <div class="header-column justify-content-end d-none d-lg-flex">
             <div class="header-row">
               <ul class="header-social-icons social-icons social-icons-clean social-icons-icon-light social-icons-big d-lg-flex m-0 ml-lg-2">
-                <li class="social-icons-instagram"><a href="http://www.instagram.com/" target="_blank" class="text-4" title="Instagram"><i class="fab fa-instagram"></i></a></li>
-                <li class="social-icons-twitter"><a href="http://www.twitter.com/" target="_blank" class="text-4" title="Twitter"><i class="fab fa-twitter"></i></a></li>
-                <li class="social-icons-facebook"><a href="http://www.facebook.com/" target="_blank" class="text-4" title="Facebook"><i class="fab fa-facebook-f"></i></a></li>
+                <li class="social-icons-instagram"><a rel="noreferrer" href="http://www.instagram.com/" target="_blank" class="text-4" title="Instagram"><i class="fab fa-instagram"></i></a></li>
+                <li class="social-icons-twitter"><a rel="noreferrer" href="http://www.twitter.com/" target="_blank" class="text-4" title="Twitter"><i class="fab fa-twitter"></i></a></li>
+                <li class="social-icons-facebook"><a rel="noreferrer" href="http://www.facebook.com/" target="_blank" class="text-4" title="Facebook"><i class="fab fa-facebook-f"></i></a></li>
               </ul>
             </div>
           </div>
